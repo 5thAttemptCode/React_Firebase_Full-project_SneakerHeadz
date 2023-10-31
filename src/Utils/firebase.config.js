2c73b9de3d326914a73f5a969fa7e510d1e4f68d
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-10-31 10:04:00 +0100

    Update firebase.config.js

--- src/Utils/firebase.config.js
+++ src/Utils/firebase.config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbJDZqPBCXFzejDxo9pLqdf-licftFUWc",
-  authDomain: "sneaker-ae167.firebaseapp.com",
-  projectId: "sneaker-ae167",
-  storageBucket: "sneaker-ae167.appspot.com",
-  messagingSenderId: "673100696199",
-  appId: "1:673100696199:web:a9bd4c85d006a02d130622"
+  apiKey: "YOUR APIKEY",
+  authDomain: "YOUR ATUODOMAIN",
+  projectId: "YOUR PROJECTID",
+  storageBucket: "YOUR DATA",
+  messagingSenderId: "YOUR DATA",
+  appId: "YOUR DATA"
 };
 
 
